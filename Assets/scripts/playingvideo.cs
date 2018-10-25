7492ae748137476502161ab5612f00000259fbb3
Moamen_Elsheikh <40501321+MoamenAliHassan@users.noreply.github.com>
2018-10-25 16:10:35 +0200

    Night At The Museum

---
+++ Assets/scripts/playingvideo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playingvideo : MonoBehaviour {
+	public GameObject outSound;
+	public GameObject insideSound;
+	public GameObject playVideo;
+
+
+	// Use this for initialization
+	void Start () {
+		outSound.SetActive (true);
+		insideSound.SetActive (false);
+		playVideo.SetActive (false);
+	}
+
+	public void OnTriggerEnter(Collider Other){
+		if(Other.gameObject.name == "coll"){
+			Debug.Log ("I'm here");
+			outSound.SetActive (false);
+			insideSound.SetActive (true);
+			playVideo.SetActive (true);
+		
+
+		}
+	}
+
+	public void OnTriggerExit(Collider Other){
+		if(Other.gameObject.name == "coll"){
+			outSound.SetActive (true);
+			insideSound.SetActive (false);
+			playVideo.SetActive (false);
+
+
+		}
+	}
+}
---
+++ README.txt
@@ -0,0 +1,9 @@
+Unity Version:[Unity LTS Release 2017.4.4]
+GVR SDK for Unity: v1.100.1
+
+my name: Moamen Ali Hassan 
+summary of project:The user is moving in the museum and heshov all the solar planets and he is explaining them.
+
+Medium Article Link:https://medium.com/@moamenali/night-at-museum-project-f2c21877db5a
+
+
